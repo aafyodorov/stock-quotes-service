93623d829723b4c76fab167a4bc02de9dd0d6234
fgavin <fgavin@student.21-school.ru>
2021-03-20 11:48:43 +0300

    Fix RestResponseEntityExceptionHandler. Change readme.

--- src/main/java/tk/stockquotesservice/exception/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/tk/stockquotesservice/exception/handlers/RestResponseEntityExceptionHandler.java
@@ -4,14 +4,15 @@ import feign.FeignException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import tk.stockquotesservice.exception.GenericFeignException;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,16 +27,33 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
   Logger logger = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
 
   @ExceptionHandler(value = {FeignException.class})
-  public ResponseEntity<JSONObject> handleFeignClientExceptions(FeignException ex, WebRequest request) {
-    Pattern messagePattern = Pattern.compile("\\[(.*)] during \\[(.*)] to \\[(.*)] \\[(.*)]: \\[(.*)]");
-    Matcher messageMatcher = messagePattern.matcher(ex.getMessage());
-    JSONObject responseBody = new JSONObject();
-    if (messageMatcher.find()) {
-    } else {
-      logger.error("In {}.class ", RestResponseEntityExceptionHandler.class.getName());
-//      responseBody.put("message", "")
-      throw ex;
-    }
-    return null;
+  public ResponseEntity<Object> handleFeignClientExceptions(FeignException ex, WebRequest request) {
+	Pattern messagePattern = Pattern.compile("\\[(.*)] during \\[(.*)] to \\[(.*)] \\[(.*)]: \\[(.*)]");
+	Matcher messageMatcher = messagePattern.matcher(ex.getMessage());
+	JSONObject responseBody = new JSONObject();
+	HttpHeaders headers = new HttpHeaders();
+	int status;
+	String statusStr;
+
+	headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+	if (messageMatcher.find()) {
+	  int firstSpace = messageMatcher.group(1).indexOf(' ');
+	  if (firstSpace != -1) {
+		statusStr = messageMatcher.group(1).substring(0, firstSpace);
+		responseBody.put("reason", messageMatcher.group(1).substring(firstSpace + 1));
+	  } else {
+		statusStr = messageMatcher.group(1);
+	  }
+	  status = Integer.parseInt(statusStr);
+	  responseBody.put("status", status);
+	  responseBody.put("message", messageMatcher.group(5));
+	} else {
+	  logger.error("In [{}.class] Feign message pattern doesn't match the exception message.",
+			  RestResponseEntityExceptionHandler.class.getName());
+	  responseBody.put("status", 500);
+	  responseBody.put("message", "Error when requesting IEXCloud.");
+	  status = 500;
+	}
+	return handleExceptionInternal(ex, responseBody.toString(), headers, HttpStatus.valueOf(status), request);
   }
 }
---
+++ src/test/java/tk/stockquotesservice/exception/handlers/RestResponseEntityExceptionHandlerTest.java
@@ -0,0 +1,64 @@
+package tk.stockquotesservice.exception.handlers;
+
+import feign.FeignException;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class RestResponseEntityExceptionHandlerTest {
+
+  @Test
+  public void handleFeignClientExceptions_FeignPatternNotMatch() {
+	FeignException ex = Mockito.mock(FeignException.class);
+	WebRequest request = Mockito.mock(WebRequest.class);
+	when(ex.getMessage()).thenReturn("Message not matching pattern");
+	RestResponseEntityExceptionHandler entityExceptionHandler = new RestResponseEntityExceptionHandler();
+
+	ResponseEntity<Object> responseEntity = entityExceptionHandler.handleFeignClientExceptions(ex, request);
+	assertEquals(responseEntity.getStatusCodeValue(), 500);
+	String body = (String) responseEntity.getBody();
+	assertNotNull(body);
+	assertTrue(body.contains("Error when requesting IEXCloud."));
+	assertTrue(body.contains("message"));
+  }
+
+  @Test
+  public void handleFeignClientExceptions_CorrectPatternWithReason() {
+	FeignException ex = Mockito.mock(FeignException.class);
+	WebRequest request = Mockito.mock(WebRequest.class);
+	RestResponseEntityExceptionHandler entityExceptionHandler = new RestResponseEntityExceptionHandler();
+
+	when(ex.getMessage())
+			.thenReturn("[404 Not Found] during [GET] to [https://www.correct-uri] " +
+					"[IEXClient#getCompany(String,String)]: [Unknown symbol]");
+	ResponseEntity<Object> responseEntity = entityExceptionHandler.handleFeignClientExceptions(ex, request);
+	assertEquals(responseEntity.getStatusCodeValue(), 404);
+	String body = (String) responseEntity.getBody();
+	assertNotNull(body);
+	assertTrue(body.contains("status"));
+	assertTrue(body.contains("message"));
+	assertTrue(body.contains("\"reason\":\"Not Found\""));
+  }
+
+  @Test
+  public void handleFeignClientExceptions_CorrectPatternWithoutReason() {
+	FeignException ex = Mockito.mock(FeignException.class);
+	WebRequest request = Mockito.mock(WebRequest.class);
+	RestResponseEntityExceptionHandler entityExceptionHandler = new RestResponseEntityExceptionHandler();
+
+	when(ex.getMessage())
+			.thenReturn("[404] during [GET] to [https://www.correct-uri] " +
+					"[IEXClient#getCompany(String,String)]: [Unknown symbol]");
+	ResponseEntity<Object> responseEntity = entityExceptionHandler.handleFeignClientExceptions(ex, request);
+	assertEquals(responseEntity.getStatusCodeValue(), 404);
+	String body = (String) responseEntity.getBody();
+	assertNotNull(body);
+	assertTrue(body.contains("status"));
+	assertFalse(body.contains("reason"));
+	assertTrue(body.contains("message"));
+  }
+}
